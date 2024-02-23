e4fbf482bac4c99bc04cddedd544dbd0917597b0
Jairo Rodriguez <rodriguezcastrojairo13@gmail.com>
2024-02-22 22:38:19 -0600

    Create the workshop 3

--- Workshop 2/script.js
+++ Workshop 2/script.js
@@ -1,23 +1,26 @@
-$(document).ready(function(){
-    $('.ver-mas').click(function(e){
-        e.preventDefault();
-        
-        // Obtener el elemento full-content asociado a la noticia actual
-        var fullContent = $(this).siblings('.full-content');
+$(document).ready(function () {
+  $(".ver-mas").click(function (e) {
+    e.preventDefault();
 
-        // Ocultar todas las noticias completas excepto la actual
-        $('.full-content').not(fullContent).addClass('d-none');
+    // Obtener el elemento full-content asociado a la noticia actual
+    var fullContent = $(this).siblings(".full-content");
 
-        // Mostrar o ocultar la noticia actual según su estado actual
-        fullContent.toggleClass('d-none');
+    // Ocultar todas las noticias completas excepto la actual
+    $(".full-content").not(fullContent).addClass("d-none");
 
-        // Ajustar el tamaño de la imagen de la tarjeta clicada
-        var cardBody = $(this).closest('.card-body');
-        cardBody.toggleClass('expanded-card');
+    // Mostrar o ocultar la noticia actual según su estado actual
+    fullContent.toggleClass("d-none");
 
-        // Desplazarse a la posición de la tarjeta clicada
-        $('html, body').animate({
-            scrollTop: cardBody.offset().top
-        }, 1000);
-    });
+    // Ajustar el tamaño de la imagen de la tarjeta clicada
+    var cardBody = $(this).closest(".card-body");
+    cardBody.toggleClass("expanded-card");
+
+    // Desplazarse a la posición de la tarjeta clicada
+    $("html, body").animate(
+      {
+        scrollTop: cardBody.offset().top,
+      },
+      1000
+    );
+  });
 });
